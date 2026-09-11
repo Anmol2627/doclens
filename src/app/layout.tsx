3b225bb946e4110e789753cdb625d1cd039aa37f
Anmol <anmol261027@gmail.com>
2026-09-11 09:22:37 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Doctor Dok",
+  title: "DocLens",
   description: "End 2 End encrypted vault for your most precious data",
 };
 
